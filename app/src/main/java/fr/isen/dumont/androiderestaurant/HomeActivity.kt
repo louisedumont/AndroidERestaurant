f5b506d7a265b3c95dd2f63dfc7c8eeda6c06054
Louise Dumont <louise.dumont03@outlook.fr>
2024-02-06 16:28:37 +0100

    Avancement sur la mise en page du site

--- app/src/main/java/fr/isen/dumont/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/dumont/androiderestaurant/HomeActivity.kt
@@ -5,9 +5,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.ui.Alignment as ComposeAlignment
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +26,8 @@ import fr.isen.dumont.androiderestaurant.ui.theme.AndroidERestaurantTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -56,7 +65,7 @@ class HomeActivity : ComponentActivity(), MenuInterface {
 
 //@PreviewParameter(MenuInterface::class)
 //interface MenuInterface {
-  //  fun dishPressed(dishsType: DishsType)
+//  fun dishPressed(dishsType: DishsType)
 //}
 
 @Preview(showBackground = true)
@@ -69,42 +78,100 @@ fun GreetingPreview() {
 
 @Composable
 fun Greeting(menu: MenuInterface) {
-    Column {
-        //Image(painterResource(R.drawable.ic_launcher_foreground), null)
-        Button(onClick = {menu.dishPressed(DishsType.STARTER) }) {
+    //definir le fond couleur boutons et les centrer
+    //val buttonBackgroundColor = Color.White
+    //val buttonModifier = Modifier
+       // .background(color = buttonBackgroundColor)
+        //.align(Alignment.CenterVertically)
+    Column ( verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize())
+    {
+        Box(modifier = Modifier.
+        padding(16.dp)
+            .fillMaxSize()
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.menuitalian),
+                contentDescription = "Photo menu app",
+                modifier = Modifier
+                    .border(width = 3.dp, color = Color.Gray)
+                    .clip(RoundedCornerShape(10.dp))
+                    .padding((8.dp))
+            )
+            //Image(painterResource(R.drawable.ic_launcher_foreground), null)
+
+
+            Text(
+                text = "Benvenuti chez ItaloResto!",
+                //modifier = Modifier.padding(24.dp)
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+            )
+        }
+
+        ButtonRow(menu = menu)
+    }
+}
+
+
+
+
+
+//definir la fonction pour les boutons
+@Composable
+fun ButtonRow(menu: MenuInterface){
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth())
+    {
+
+        Button(onClick = {menu.dishPressed(DishsType.STARTER) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Gray,
+                contentColor = Color.White,
+            )
+            )
+        {
             //Text("Entrees") de cette manière ce n'est pas très adapté il faut appeler les strings dans le fichier .xml
             Text(stringResource(R.string.menu_starter))
         }
-        Button(onClick = {menu.dishPressed(DishsType.MAIN)}) {
+        Button(onClick = {menu.dishPressed(DishsType.MAIN) },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray,
+                contentColor = Color.White))
+        {
             //Text("Plats")
             Text(stringResource(R.string.menu_main))
 
         }
-        Button(onClick = {menu.dishPressed(DishsType.DESSERT)}) {
+        Button(onClick = {menu.dishPressed(DishsType.DESSERT) },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Gray,
+                contentColor = Color.White))
+        {
             //Text("Desserts")
             Text(stringResource(R.string.menu_dessert))
 
         }
-
-        Surface(color = Color.Blue) {
-            Text(text = "Bienvenue chez ItaloResto!",
-                //modifier = Modifier.padding(24.dp)
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    Image(painter = painterResource(id = R.drawable.menuitalian),
-          contentDescription = "Photo menu app",
-          modifier = Modifier
-              .border(width = 3.dp, color = Color.Blue)
-              .clip(RoundedCornerShape(10.dp))
-              .padding((8.dp)
-
-
-
-            ))
-
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
