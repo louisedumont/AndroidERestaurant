fbf768ff5079b0412b253722a0b0d85044e7c0b4
Louise Dumont <louise.dumont03@outlook.fr>
2024-02-12 11:45:29 +0100

    cours ajout code dans menuactivity pour accès API

--- app/src/main/java/fr/isen/dumont/androiderestaurant/MenuActivity.kt
+++ app/src/main/java/fr/isen/dumont/androiderestaurant/MenuActivity.kt
@@ -5,17 +5,57 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.android.volley.Request.Method
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import fr.isen.dumont.androiderestaurant.network.NetworkConstants
+import org.json.JSONObject
 
 class MenuActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MenuView()
         }
+        Log.d("lifeCycle", "Menu Activity - OnCreate")
+    }
+
+    override fun onPause() {
+        Log.d("lifeCycle", "Menu Activity - OnPause")
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifeCycle", "Menu Activity - OnResume")
     }
 
     override fun onDestroy() {
-        Log.d("Lifecycle", "Menu Activity - onDestroy")
+        Log.d("lifeCycle", "Menu Activity - onDestroy")
         super.onDestroy()
     }
 }
+
+@Composable
+fun MenuView() {
+    val context = LocalContext.current
+    val queue= Volley.newRequestQueue(context)
+    val params = JSONObject()
+    params.put(NetworkConstants.ID_SHOP, 1)
+    val request = JsonObjectRequest(
+        Method.POST,
+        NetworkConstants.URL,
+        params,
+        {
+
+        },
+        {
+
+        }
+    )
+    queue.add(request)
+}
+
+
---
+++ app/src/main/java/fr/isen/dumont/androiderestaurant/network/NetworkConstants.kt
@@ -0,0 +1,11 @@
+package fr.isen.dumont.androiderestaurant.network
+
+import java.net.URL
+
+class NetworkConstants {
+    companion object{
+        val URL = "http://test.api.catering.bluecodegames.com/menu"
+        var ID_SHOP = "id_shop"
+    }
+}
+
